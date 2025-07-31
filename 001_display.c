268edec63b6335184814f4985860bdc682a6069f
Cameo <39931114+cameo691@users.noreply.github.com>
2025-07-31 14:10:58 +0000

    Add useful comments

--- 001_display.c
+++ 001_display.c
@@ -2,7 +2,10 @@
 
 int main() {
     
+    // Displays "I like strawberries!" in terminal
     printf("I like strawberries!\n");
+
+    // Displays "It's really good!" in terminal
     printf("It's really good!\n");
     
     return 0;
